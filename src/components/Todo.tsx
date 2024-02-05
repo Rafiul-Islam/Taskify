74ef1d13491f33c18fcfb1f18f5ca4b71fddc84c
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-02-05 19:38:09 +0600

    feature - add todo delete and is complete functional

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,5 @@
 import TodoModal from "../model/todo.ts";
-import React from "react";
+import React, {FormEvent, useState} from "react";
 
 interface PropsType {
     todo: TodoModal,
@@ -7,15 +7,39 @@ interface PropsType {
     setTodos: React.Dispatch<React.SetStateAction<TodoModal[]>>
 }
 
-const Todo = ({todo}: PropsType) => {
+const Todo = ({todo, todos, setTodos}: PropsType) => {
+    const {id, todo: todoTask, isCompleted} = todo;
+    const [isEditable, setIsEditable] = useState<boolean>(false)
+    const handleComplete = (todoId: string) => {
+        const updatedTodos = todos.map(todo => {
+            if (todo.id === todoId) {
+                todo.isCompleted = !todo.isCompleted
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
+    const handleDelete = (todoId: string) => {
+        const updatedTodos = todos.filter(todo => todo.id !== todoId);
+        setTodos(updatedTodos);
+    }
+    const handleEdit = (todoId: string) => {
+        console.log(todoId);
+        setIsEditable(true);
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        setIsEditable(false);
+    }
     return (
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
             <li className='todo-item'>
-                <p contentEditable className='todo-task'> {todo.todo} </p>
+                <p contentEditable={isEditable} className={`todo-task ${isCompleted && 'completed'}`}> {todoTask} </p>
                 <div className='button-container'>
-                    <i role='button' className='fa fa-pencil'></i>
-                    <i role='button' className='fa fa-trash'></i>
-                    <i role='button' className='fa fa-check'></i>
+                    <i role='button' className='fa fa-pencil' onClick={() => handleEdit(id)}></i>
+                    <i role='button' className='fa fa-trash' onClick={() => handleDelete(id)}></i>
+                    <i role='button' className='fa fa-check' onClick={() => handleComplete(id)}></i>
                 </div>
             </li>
         </form>
